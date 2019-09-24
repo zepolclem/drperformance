c81c1fbbe804e9264dfb170e261a0a7e57cd6655
Clement Lopez <lopez.clmnt@gmail.com>
2019-09-24 21:01:22 +0200

    users entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $admin = new User();
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'projectgtr'
+        ));
+
+        $manager->flush();
+    }
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+}
